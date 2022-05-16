9ffa82ffb636210137171595e39aaa398ae90947
admarath <admarath@LIN51015617.corp.capgemini.com>
2022-05-16 08:41:59 +0530

    config files added

---
+++ BugTracker/src/main/java/User.java
@@ -0,0 +1,15 @@
+
+import java.util.Date;
+
+public class User implements java.io.Serializable {
+
+	private Long userid;
+	private String name;
+	private String username;
+	private String password;
+	private String email;
+	private String phone;
+	
+
+	//getter and setter methods
+}
